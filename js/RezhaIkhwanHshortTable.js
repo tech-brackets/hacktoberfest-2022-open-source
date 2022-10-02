bb6e5a69cec39634cd88d908fdd40701302fde39
RezhaIkhwan.H <rezhaihwanhidayat97@gmail.com>
2022-10-02 15:10:18 +0700

    tambahin file short table

--- js/RezhaIkhwanHshortTable.js
+++ js/RezhaIkhwanHshortTable.js
@@ -1,3 +1,15 @@
+
+//cara pake nya pangil olclic di html bagia th dan beri index th ke berapa dimulai dari 0
+// contoh
+//<table id="myTable">
+{/* <thead>
+    <tr>
+        <th onclick="sort(1)">nama</th>
+        <th onclick="sort(2)">tanggal</th>
+        <th onclick="sort(3)">waktu</th>
+        <th onclick="sort(4)">lokasi</th>
+    </tr>
+// </thead> */}
 function sort(val) {
     const index = (isNaN(val) ? val.value : val) - 1
     const tabel = document.getElementById('myTable')
